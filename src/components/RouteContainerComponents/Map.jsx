7b75c2696128496d5cd3618bb9ab8832ad7e36aa
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-25 12:02:49 +0100

    called fetchRouteWaypointsList

--- src/components/RouteContainerComponents/Map.jsx
+++ src/components/RouteContainerComponents/Map.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
 
 const Map = () => {
-    const [ routeWaypointsList, setRouteWaypointsList] = useState ([]);
+  const [routeWaypointsList, setRouteWaypointsList] = useState([]);
 
-    const fetchRouteWaypointsList = async () => {
-        const response = await fetch ("http://localhost:8080/routes/all/waypoints")
-        const data = await response.json()
-        setRouteWaypointsList(data);
-    }
+  const fetchRouteWaypointsList = async () => {
+    const response = await fetch("http://localhost:8080/routes/all/waypoints");
+    const data = await response.json();
+    setRouteWaypointsList(data);
+  };
 
-    // wait to output routeWaypointsList
-    useEffect(() => {
-        console.log(routeWaypointsList)
-    }, [routeWaypointsList])
+  useEffect(() => {
+    fetchRouteWaypointsList();
+  }, []);
 
-}
+  // wait to output routeWaypointsList
+  useEffect(() => {
+    console.log(routeWaypointsList);
+  }, [routeWaypointsList]);
 
+  return <></>;
+};
+
+export default Map;
---
+++ src/containers/RouteContainer.jsx
@@ -0,0 +1,11 @@
+import Map from "../components/RouteContainerComponents/Map";
+
+const RouteContainer = () => {
+  return (
+    <>
+      <Map />
+    </>
+  );
+};
+
+export default RouteContainer;
