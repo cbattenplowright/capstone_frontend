06c340ceebd37c1ac00e8eb65497aa9d241f5c4d
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-26 11:16:32 +0100

    route

--- src/components/RouteContainerComponents/Route.jsx
+++ src/components/RouteContainerComponents/Route.jsx
@@ -1,6 +1,12 @@
-const Route = () => {
+const Route = ({route}) => {
+
     return (
-        <></>
+        <div id="route">
+            <p> {route.routeName} </p>
+            <p> {route.distance} </p>
+            <p> 3 hours </p>
+            <p> {route.orders.length} </p>
+        </div>
     )
 }
 
