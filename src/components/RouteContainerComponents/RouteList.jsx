58f12fd8673e1139a28d08edb7eade12d0b295fd
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-26 11:05:04 +0100

    routeList

--- src/components/RouteContainerComponents/RouteList.jsx
+++ src/components/RouteContainerComponents/RouteList.jsx
@@ -1,7 +1,11 @@
-const RouteList = () => {
-    return (
-        <></>
-    )
-}
+import Route from "./Route";
+
+const RouteList = ({ routes }) => {
+  const routeComponents = routes.map((route) => {
+    return <Route key={route.id} route={route} />;
+  });
+
+  return <div id="route-list">{routeComponents}</div>;
+};
 
 export default RouteList;
