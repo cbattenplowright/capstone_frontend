ed70cc916aaf1a095907a2dba2776d18fca93f59
mikeycb96 <michaelsonbaptist@gmail.com>
2023-09-27 14:15:59 +0100

    OrderList

--- src/components/OrderContainerComponents/OrderList.jsx
+++ src/components/OrderContainerComponents/OrderList.jsx
@@ -1,11 +1,40 @@
 import Order from "./Order";
 
-
-const OrderList = () => {
+const OrderList = ({
+  orders,
+  addToSelectedOrderList,
+  removeFromSelectedOrderList,
+}) => {
+  const orderComponents = orders.map((order) => {
     return (
-        <>
-        </>
-    )
-}
+      <tr> 
+      <Order
+        key={order.id}
+        order={order}
+        addToSelectedOrderList={addToSelectedOrderList}
+        removeFromSelectedOrderList={removeFromSelectedOrderList}
+      />
+      </tr>  
+    );
+  });
+
+  return (
+    <div id="order-list">
+      <table>
+        <caption>ORDERS</caption>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Order Name</th>
+            <th>Order Id</th>
+            <th>Delivery Address</th>
+            <th>Current Route</th>
+          </tr>
+        </thead>
+        <tbody>{orderComponents}</tbody>
+      </table>
+    </div>
+  );
+};
 
 export default OrderList;
