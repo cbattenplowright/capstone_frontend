026b623ba4607f3928a612dd5b5361f4911a3683
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-27 14:50:02 +0100

    added react router skeleton to website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import RouteContainer from "./containers/RouteContainer";
 import OrderContainer from "./containers/OrderContainer";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <main>
-        <OrderContainer />
+        <Routes>
+          <Route path="/" element={<RouteContainer />} />
+          <Route path="/orders" element={<OrderContainer />} />
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
 
