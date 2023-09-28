0d7bae9438806444600000fc0fc1cd560c9a4465
rohaib1 <rohaib192@gmail.com>
2023-09-28 09:52:13 +0100

    Added a delivered button on the modal

--- src/components/RouteContainerComponents/OrderModal.jsx
+++ src/components/RouteContainerComponents/OrderModal.jsx
@@ -2,8 +2,17 @@
 import React from "react";
 import "./OrderModal.css";
 
-
 const OrderModal = ({ order, closeModal }) => {
+
+  const updateDeliveryStatus = async () => {
+    const response = await fetch(`http://localhost:8080/orders/${order.id}?status=true`, 
+    {
+      method: "PATCH"
+    })
+    const json = await response.json();
+    console.log(json);
+  }
+
   return (
     <div className="modal">
       <div className="modal-content">
@@ -14,6 +23,7 @@ const OrderModal = ({ order, closeModal }) => {
         <p>Address: {order.address}</p>
         <p>Co-ordinates: {order.lat}, {order.lng}</p>
         <button onClick={closeModal}>Close</button>
+        <button onClick={updateDeliveryStatus}>Delivered!</button>
       </div>
     </div>
   );
