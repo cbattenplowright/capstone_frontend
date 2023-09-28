1224191698db01d5a3ecdaf4af2d5bba8428c47a
rohaib1 <rohaib192@gmail.com>
2023-09-28 10:35:20 +0100

    When delievered button is pressed it goes green

--- src/components/RouteContainerComponents/OrderModal.jsx
+++ src/components/RouteContainerComponents/OrderModal.jsx
@@ -1,18 +1,20 @@
-// OrderModal.jsx
-import React from "react";
+import React, { useState } from "react";
 import "./OrderModal.css";
 
 const OrderModal = ({ order, closeModal }) => {
+  const [statusUpdated, setStatusUpdated] = useState(false);
 
   const updateDeliveryStatus = async () => {
-    const response = await fetch(`http://localhost:8080/orders/${order.id}?status=true`, 
-    {
-      method: "PATCH"
-    })
+    const response = await fetch(
+      `http://localhost:8080/orders/${order.id}?status=true`,
+      {
+        method: "PATCH"
+      }
+    );
     const json = await response.json();
     console.log(json);
-    alert("Delivered!")
-  }
+    setStatusUpdated(true); // Update the status when the button is clicked
+  };
 
   return (
     <div className="modal">
@@ -24,7 +26,11 @@ const OrderModal = ({ order, closeModal }) => {
         <p>Address: {order.address}</p>
         <p>Co-ordinates: {order.lat}, {order.lng}</p>
         <button onClick={closeModal}>Close</button>
-        <button onClick={updateDeliveryStatus}>Delivered!</button>
+        {statusUpdated ? (
+          <button style={{ color: "green" }}>✔ Delivered!</button>
+        ) : (
+          <button onClick={updateDeliveryStatus}>Delivered!</button>
+        )}
       </div>
     </div>
   );
